42924316a0278953c48f85852d8257c46381e326
Andy Williams <andy@nonissue.org>
2020-03-18 18:31:02 -0600

    Basic layout with new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.css";
+import logo512 from "./logo512.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">nonissue.org</header>
+    <div className={styles.app}>
+      <header className={styles["App-header"]}>
+        <img alt="logo" src={logo512} width="256px" height="256px" />
+        <h1>NONISSUE</h1>
+        <ul>
+          <li>
+            <a href="https://nonissue.org">Web</a>
+          </li>
+          <li>
+            <a href="https://instagram.com/nonissue">IG</a>
+          </li>
+          <li>
+            <a href="mailto:hi@nonissue.org">Mail</a>
+          </li>
+        </ul>
+      </header>
     </div>
   );
 }
